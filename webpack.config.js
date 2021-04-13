7f2a47f5f3275a4626d857c42f71e0c5b0e7dd45
rafael <rafael@IT-Admins-MacBook-Pro.local>
2021-04-13 01:06:28 -0300

    add webpack and eslint

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const ROOT = __dirname;
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    port: 3000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(ROOT, "dist"),
+  },
+
+  entry: {
+    main: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(ROOT, "public/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+};
